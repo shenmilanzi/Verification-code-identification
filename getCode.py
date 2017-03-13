e6ead3a75190f1511a449e9decd1180bf3af62ad
shenmilanzi <jiaweiwang.1992@gmail.com>
2017-03-13 19:27:16 +0800

    get hunderds of original verification code images

---
+++ getCode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import os
+from urllib.request import urlretrieve
+
+#for i in range(1,501):
+#    url = 'http://www.169ol.com/Stream/Code/getCode'
+#    path = r'C:\Users\Wjw\Pictures\imgdata'
+#    imgname = str(i) + '.png'
+#    imgpath = os.path.join(path, imgname)
+#    urlretrieve(url, imgpath)
+ 
+def getCode():
+    url = 'http://www.169ol.com/Stream/Code/getCode'
+    path = r'C:\Users\Wjw\Pictures\imgdata'
+    imgname = '501.png'
+    imgpath = os.path.join(path, imgname)
+    urlretrieve(url, imgpath)
